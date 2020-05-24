831519d25963055ca4a956352dfd324034eb4e9c
Abdelrhman Abulkasem <52038881+Abdelrhman0100@users.noreply.github.com>
2020-05-25 02:22:39 +0400

    Update finger ex2.py

--- finger ex2.py
+++ finger ex2.py
@@ -1,3 +1,6 @@
+''' using while loop write a program taking an input number of how many times to print "X"
+
+
 numXs = int(input('How many times should I print the letter X? '))
 toPrint = ''
 while(len(toPrint)<numXs):
